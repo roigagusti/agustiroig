ef8a07d1938a999549957cbc876f11a83a17c0b4
Agustí Roig <gutiroma@hotmail.com>
2019-05-10 20:32:48 +0200

    Web provisional

---
+++ index.php
@@ -0,0 +1,90 @@
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="//www.juliagnaranjo.com/css/style.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+  <!--<link rel="stylesheet" href="http://fontawesome.io/assets/font-awesome/css/font-awesome.css">-->
+  <!-- CSS custom -->
+  <!-- Google Fonts -->
+  <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Titillum+Web">
+  <!-- Scripts custom -->
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+<style>
+@font-face {
+  font-family: 'Freight Big Light';
+  src: url('//www.juliagnaranjo.com/fonts/FreightBigLight.otf'); /* IE9 Compat Modes */
+  src: url('//www.juliagnaranjo.com/fonts/FreightBigLight.otf?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+}
+@font-face {
+  font-family: 'Freight Big Light Italic';
+  src: url('//www.juliagnaranjo.com/fonts/FreightBigLightItalic.otf'); /* IE9 Compat Modes */
+  src: url('//www.juliagnaranjo.com/fonts/FreightBigLight.otfItalic?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+}
+@font-face {
+  font-family: 'Freight Text';
+  src: url('//www.juliagnaranjo.com/fonts/FreightText.otf'); /* IE9 Compat Modes */
+  src: url('//www.juliagnaranjo.com/fonts/FreightText.otfItalic?#iefix') format('embedded-opentype'), /* IE6-IE8 */
+}
+*{
+    transition: all 0.6s;
+}
+
+html {
+    height: 100%;
+}
+
+body{
+    font-weight: 300;
+    margin: 0;
+    cursor: crosshair;
+    background-color: #f5f5f5;
+    color: #000;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    -webkit-text-size-adjust: none;
+    display: inline-block;
+    padding-right: 12px;
+    animation: type .5s alternate infinite;
+}
+
+#main{
+    display: table;
+    width: 100%;
+    height: 100vh;
+    text-align: center;
+}
+
+.fof{
+	display: table-cell;
+	vertical-align: middle;
+    letter-spacing: -1.5px;
+    font-size: 50px;
+    font-family: 'Freight Text';
+    font-weight:500;
+}
+a{
+    text-decoration:none;
+    color:#c3c3c3;
+}
+a:hover{
+    color:#ff6f61;
+}
+span{
+    color:#c3c3c3;
+    font-weight:100;
+}
+
+@keyframes type{
+	from{box-shadow: inset -3px 0px 0px #000;
+	to{box-shadow: inset -3px 0px 0px transparent;}
+}
+</style>
+</head>
+<body>
+<div id="main">
+    	<div class="fof">
+        		Aldasoro. <span><a href="mailto:agusti@agustiroig.com?Subject=Contacto%20web&Body=Aprovecha%20que%20se%20ha%20abierto%20el%20mail%20para%20mandarme%20un%20saludo%20:)">Agustí Roig</a></span>
+    	</div>
+</div>
+</body>
+</html>
